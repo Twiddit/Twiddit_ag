57de9e2983b19fe6e8850f9c7f39499360e1ea63
CarlosJulian1231 <ccordero@unal.edu.co>
2023-03-26 17:58:20 -0500

    ag social

--- src/businessLogic/social/resolvers.js
+++ src/businessLogic/social/resolvers.js
@@ -1,43 +1,113 @@
 import relationshipRequests from '../../restConsumption/social/requests';
+import profileRequests from '../../restConsumption/profile/requests';
 
 const relationshipResolvers = {
     Query: {
         
-        followersAll: (_, {followedId}) => {
-            return relationshipRequests.followersAll(_, {followedId})
+        followersAll: async (_, {followedId}) => {
+            const id = followedId
+            const user = await profileRequests.viewProfile(_, { id });
+            if (user.email != undefined){
+                return relationshipRequests.followersAll(_, { followedId })
+            }
+            return null
+            
         },
-        followingAll: (_, {followerId}) => {
-            return relationshipRequests.followingAll(_, {followerId})
+        followingAll: async (_, {followerId}) => {
+            const id = followerId
+            const user = await profileRequests.viewProfile(_, { id });
+            if (user.email != undefined){
+                return relationshipRequests.followingAll(_, {followerId})
+            }
+            return null
+
         },
-        blockedAll: (_, {followedId}) => {
-            return relationshipRequests.blockedAll(_, {followedId})
+        blockedAll: async (_, {followedId}) => {
+            const id = followedId
+            const user = await profileRequests.viewProfile(_, { id });
+            if (user.email != undefined){
+                return relationshipRequests.blockedAll(_, {followedId})
+            }
+            return null
+
         },
-        numberFollowers: (_, {followedId}) => {
-            return relationshipRequests.numberFollowers(_, {followedId})
+        numberFollowers: async (_, {followedId}) => {
+            const id = followedId
+            const user = await profileRequests.viewProfile(_, { id });
+            if (user.email != undefined){
+                return relationshipRequests.numberFollowers(_, {followedId})
+            }
+            return null
+
         },
-        numberFollowing: (_, {followerId}) => {
-            return relationshipRequests.numberFollowing(_, {followerId})
+        numberFollowing: async (_, {followerId}) => {
+            const id = followerId
+            const user = await profileRequests.viewProfile(_, { id });
+            if (user.email != undefined){
+                return relationshipRequests.numberFollowing(_, {followerId})
+            }
+            return null
+
         },
-        numberBlocked: (_, {followedId}) => {
-            return relationshipRequests.numberBlocked(_, {followedId})
+        numberBlocked: async (_, {followedId}) => {
+            const id = followedId
+            const user = await profileRequests.viewProfile(_, { id });
+            if (user.email != undefined){
+                return relationshipRequests.numberBlocked(_, {followedId})
+            }
+            return null
+
         },
-        isBlocked: (_, {followedId, followerId}) => {
-            return relationshipRequests.isBlocked(_, {followedId, followerId})
+        isBlocked: async (_, {followedId, followerId}) => {
+            let id = followedId
+            const user1 = await profileRequests.viewProfile(_, { id });
+            id = followerId
+            const user2 = await profileRequests.viewProfile(_, { id });
+            if (user1.email != undefined && user2.email != undefined){
+                return relationshipRequests.isBlocked(_, {followedId, followerId})
+            }
+            return null
+
         },
-        isFollow: (_, {followedId, followerId}) => {
-            return relationshipRequests.isFollow(_, {followedId, followerId})
+        isFollow: async (_, {followedId, followerId}) => {
+            let id = followedId
+            const user1 = await profileRequests.viewProfile(_, { id });
+            id = followerId
+            const user2 = await profileRequests.viewProfile(_, { id });
+            if (user1.email != undefined && user2.email != undefined){
+                return relationshipRequests.isFollow(_, {followedId, followerId})
+            }
+            return null
+
         }            
     },
 
     Mutation: {
-        createRelationship: (_, {relationship}) => {
-            return relationshipRequests.createRelationship(_, {relationship})
+        createRelationship: async(_, {relationship}) => {
+            let id = relationship.followedId
+            const user1 = await profileRequests.viewProfile(_, { id });
+            id = relationship.followerId
+            const user2 = await profileRequests.viewProfile(_, { id });
+            if (user1.email != undefined && user2.email != undefined){
+                return relationshipRequests.createRelationship(_, {relationship})
+            }
+            return null
         },
-        disblock: (_, {followedId, followerId}) => {
-            return relationshipRequests.disblock(_, {followedId, followerId})
+        disblock: async(_, {followedId, followerId}) => {
+            const isBlocked = await relationshipResolvers.Query.isBlocked(_, {followedId, followerId})
+            if (isBlocked.blocked){
+               return relationshipRequests.disblock(_, {followedId, followerId})
+            }
+            return null
+
         },
-        unfollow: (_, {followedId, followerId}) => {
-            return relationshipRequests.unfollow(_, {followedId, followerId})
+        unfollow: async(_, {followedId, followerId}) => {
+            const isFollow = await relationshipResolvers.Query.isFollow(_, {followedId, followerId})
+            if (isFollow.follow){
+                return relationshipRequests.unfollow(_, {followedId, followerId})
+            }
+            return null
+
         },
     }
     
--- src/businessLogic/social/typeDefs.js
+++ src/businessLogic/social/typeDefs.js
@@ -20,25 +20,26 @@ export const relationshipTypeDef = `
     follow: Boolean
   }
   input RelationshipInput {
-    followerId: Int!
-    followedId: Int!
-    blocked: Boolean!
+    followerId: Int
+    followedId: Int
+    blocked: Boolean
+    message: String
   }
   `;
 
 export const relationshipQueries = `
-    followersAll(followedId: Int!): [Relationship!]
-    followingAll(followerId: Int!): [Relationship!]
-    blockedAll(followedId: Int!): [Relationship!]
-    numberFollowers(followedId: Int!): NumberFollowers!
-    numberFollowing(followerId: Int!): NumberFollowing!
-    numberBlocked(followedId: Int!): NumberBlocked!
-    isBlocked(followedId: Int!, followerId: Int!): Message!
-    isFollow(followedId: Int!, followerId: Int!): Message!
+    followersAll(followedId: Int!): [Relationship]
+    followingAll(followerId: Int!): [Relationship]
+    blockedAll(followedId: Int!): [Relationship]
+    numberFollowers(followedId: Int!): NumberFollowers
+    numberFollowing(followerId: Int!): NumberFollowing
+    numberBlocked(followedId: Int!): NumberBlocked
+    isBlocked(followedId: Int!, followerId: Int!): Message
+    isFollow(followedId: Int!, followerId: Int!): Message
 `;
 
 export const relationshipMutations= ` 
     createRelationship(relationship: RelationshipInput): Message!
-    disblock(followedId: Int!, followerId: Int!): Message!
-    unfollow(followedId: Int!, followerId: Int!): Message!
+    disblock(followedId: Int!, followerId: Int!): Message
+    unfollow(followedId: Int!, followerId: Int!): Message
 `;

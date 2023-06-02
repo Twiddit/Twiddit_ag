bf6b33aea69df98209684d9bb608ba28dc463de4
Alerodriguezmar <alerodriguezmar@unal.edu.co>
2023-06-01 22:42:38 -0500

    add find username

--- src/businessLogic/search_engine/resolvers.js
+++ src/businessLogic/search_engine/resolvers.js
@@ -1,10 +1,17 @@
 import searchRequests from '../../restConsumption/search_engine/requests';
+import profileResolvers from '../profile/resolvers';
 
 const searchResolvers = {
     Query: {
         
-        searchTwiddit: (_, {text}) => {
-            return searchRequests.searchTwiddit(_, {text})
+        searchTwiddit: async (_, {text}) => {
+             let twidditList =  await searchRequests.searchTwiddit(_, { text });
+           
+             for (let i = 0; i < twidditList.length; i++) {
+                const data = await profileResolvers.Query.viewProfile(_, { id: twidditList[i].userId });
+                twidditList[i]["user"] =  data.username;
+            }
+            return twidditList;
         },
         searchUser: (_, {text}) => {
             return searchRequests.searchUser(_, {text})
